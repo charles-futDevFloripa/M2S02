5b1a28fa82118e496d16b4bdd6e96e3db148e7a0
charlesbiveu <cbiveu@gmail.com>
2024-05-27 21:29:21 -0300

    resolução do exercicio 02

--- index.js
+++ index.js
@@ -15,3 +15,38 @@ function filtrarPares(numeros) {
 
 let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log(filtrarPares(numeros));
+
+////////////////////////// EXERCICIO 2 //////////////////////////
+
+/*
+Dado um array de objetos representando pessoas com propriedades nome e idade, crie uma função que retorne um array, contendo apenas os nomes das pessoas com idade maior ou igual a 18 anos.
+
+‌
+
+function filtrarAdultos(pessoas) {
+// Sua implementação aqui
+}
+
+let pessoas = [
+{ nome: 'Alice', idade: 17 },
+{ nome: 'Bob', idade: 22 },
+{ nome: 'Charlie', idade: 16 },
+{ nome: 'David', idade: 19 }
+];
+console.log(filtrarAdultos(pessoas)); // ['Bob', 'David']
+*/
+
+function filtrarAdultos(pessoas) {
+  return pessoas
+    .filter((pessoa) => pessoa.idade >= 18)
+    .map((pessoa) => pessoa.nome);
+}
+
+let pessoas = [
+  { nome: 'Alice', idade: 17 },
+  { nome: 'Bob', idade: 22 },
+  { nome: 'Charlie', idade: 16 },
+  { nome: 'David', idade: 19 },
+];
+
+console.log(filtrarAdultos(pessoas)); // ['Bob', 'David']
